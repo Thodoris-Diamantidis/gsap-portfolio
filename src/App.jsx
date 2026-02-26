cbc8e87d14f1e59695887c6abf36ea1364f78677
Thodoris-Diamantidis <thodoris_diamantidis@hotmail.com>
2026-02-26 15:48:31 +0200

    added info

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import { ScrollTrigger, SplitText} from "gsap/all";
 
 import {DownloadIcon} from "@/components/icons/lucide-download.jsx";
 import {ExternalLinkIcon} from "@/components/icons/lucide-external-link.jsx";
+import {GithubIcon} from "@/components/icons/lucide-github.jsx";
+import {FacebookIcon} from "@/components/icons/lucide-facebook.jsx";
+import {InstagramIcon} from "@/components/icons/lucide-instagram.jsx";
+import {LinkedinIcon} from "@/components/icons/lucide-linkedin.jsx";
+import {TwitterIcon} from "@/components/icons/lucide-twitter.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -30,7 +35,7 @@ const App = () => {
             </div>
 
             {/* Navbar */}
-            <nav className="fixed w-full z-20 flex justify-between items-center h-32">
+            <nav className="fixed w-full z-50 flex justify-between items-center h-32">
                 {/* Left Side */}
                 <button
                     type="button"
@@ -65,6 +70,56 @@ const App = () => {
                     </button>
                 </div>
             </nav>
+
+            <section className="w-full">
+                <div id="Hero" className="relative flex flex-col lg:flex-row h-screen w-full items-center justify-center lg:justify-between z-20 px-3 lg:px-10 overflow-hidden">
+                    <div className="hidden lg:flex flex-col gap-6 border px-5 py-10 rounded-full border-amber-300/70 bg-amber-300/10 shadow-lg shadow-amber-300/30">
+                        <a className="btn-hero"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                            href="https://github.com/Thodoris-Diamantidis"
+                        >
+                            <GithubIcon className='w-9 h-9'/>
+                        </a>
+                        <a className="btn-hero"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                            <LinkedinIcon className='w-9 h-9'/>
+                        </a>
+                        <a className="btn-hero"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                            <TwitterIcon className='w-9 h-9'/>
+                        </a>
+                        <a className="btn-hero"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                            <FacebookIcon className='w-9 h-9'/>
+                        </a>
+                        <a className="btn-hero"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                            <InstagramIcon className='w-9 h-9'/>
+                        </a>
+                    </div>
+                    <div className="flex-1 flex flex-col gap-6 justify-center text-center z-30 max-w-2xl px-5 lg:px-0 xl:px-5">
+                        <div>
+                            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-wider">
+                                Thodoris Diamantidis
+                            </h1>
+                            <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold bg-clip-text text-transparent bg-linear-to-r from-indigo-400 via-rose-400 to-amber-400 p-1">Software Engineer</h2>
+                        </div>
+                        <div>
+                            <p className="text-sm md:text-lg lg:text-xl my-5 max-w-150 leading-relaxed">I craft immersive digital experiences that blend engineering depth with creative clarity. As a <span className="text-transparent bg-clip-text font-bold bg-linear-to-r from-indigo-400 via-rose-400 to-amber-400 ">Full Stack Engineer</span>, I turn complex ideas into elegant, high‑performance solutions.</p>
+                        </div>
+                    </div>
+                    <div>test</div>
+                </div>
+            </section>
         </>
     )
 }
